4ba337a7b90f361e4fff7887020cd7e2ce761f87
Pamela Faria <pamelafaria.bg080@academico.ifsul.edu.br>
2021-04-01 01:01:43 -0300

    index.js modificado de Olá Mundo para Olá Pamela

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get("/",function(rec,res){
+    res.send("Olá Pamela!")    
+})
+
 app.listen(3000,function(){
     console.log("conexão inicial na porta 3000")
 })
