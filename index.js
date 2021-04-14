474a67362d0152c523bfcc0b7f0121eaa3271fd3
Pamela Faria <pamelamrfaria@gmail.com>
2021-04-14 20:07:28 -0300

    usando ejs nas paginas

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ app.set("view engine","ejs")
 
 app.use(express.static(path.join(__dirname,"public")))
 
+app.get("/usuarios",function(rec,res){
+    res.render('usuarios.ejs',{usuarios:[
+        {nome:'Pamela',email:'pamela096@gmail.com'},
+        {nome:'Paulo',email:'paulo125@gmail.com'},
+        {nome:'Marcia',email:'marcia123@gmail.com'},
+        {nome:'Andrio',email:'andrio675@gmail.com'}
+    ]})    
+})
 app.get("/",function(rec,res){
-    res.send("Olá Pamela!")    
+    res.render('index.ejs',{})    
 })
 
 app.listen(3000,function(){
